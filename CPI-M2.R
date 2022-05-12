14722c76f5eac92301338c04ab4db2039a4defaf
Matt Love <89164524+mlove-19@users.noreply.github.com>
2022-05-12 10:20:25 -0400

    Added seasonal examination of M2

--- CPI-M2.R
+++ CPI-M2.R
@@ -20,14 +20,101 @@ y$cpi = ts(y$cpi, frequency=12, start=c(2012, 1))
 y$M2 = ts(y$M2, frequency=12, start=c(2012, 1))
 plot.ts(y)
 
+<<<<<<< Updated upstream
+=======
+# Plotting both time series
+plot.ts(y$CPI, main = "Consumer Price Index For All Urban Consumers", ylab = "CPIAUC", col = "red")
+plot.ts(y$M2, main = "M2 Money Supply", ylab = "M2 (Billions)", col = "blue")
+
+#################
+# Examining CPI #
+#################
+
+# Mean and standard deviation of CPI ts
+mean(y$CPI)
+sd(y$CPI)
+
+#Autocorrelation Plot
+acf(y$CPI, type = "correlation", main = "Autocorrelation of CPIAUC", lag.max = 40) 
+
+#Additive or Multiplicative 
+#ADD
+y_decomp = decompose(y$CPI, type = "additive")
+
+
+#MULT
+y_decomp_mult = decompose(y$CPI, type = "multiplicative")
+
+plot(y_decomp_mult$trend)
+plot.ts(y_decomp_mult$seasonal[1:12])
+
+# MAPE
+mape = function(pred,true){
+  return(mean ( abs( (pred - true) / true ) ,na.rm=T))
+}
+
+
+y_pred_add = y_decomp$trend + y_decomp$seasonal
+y_pred_mult = y_decomp_mult$trend * y_decomp_mult$seasonal 
+
+print(mape(y_pred_add, y$CPI))
+print(mape(y_pred_mult, y$CPI))
+
+#Trend and Seasonality in Additive Plot
+plot.ts(y_decomp$trend, main = "Additive Trend Plot (CPI)", ylab = 'Trend',col = 'red')
+plot.ts(y_decomp$seasonal[1:12], main = "Additive Seasonality Plot (CPI)", ylab = 'Seasonal Effect', col = 'blue')  
+
+################
+# Examining M2 #
+################
+
+# Mean and standard deviation of M2 ts
+mean(y$M2)
+sd(y$M2)
+
+#Autocorrelation Plot
+acf(y$M2, type = "correlation", main = "Autocorrelation of M2", lag.max = 40) 
+
+#Additive or Multiplicative 
+#ADD
+y_decomp = decompose(y$M2, type = "additive")
+
+
+#MULT
+y_decomp_mult = decompose(y$M2, type = "multiplicative")
+
+plot(y_decomp_mult$trend)
+plot.ts(y_decomp_mult$seasonal[1:12])
+
+# MAPE
+mape = function(pred,true){
+  return(mean ( abs( (pred - true) / true ) ,na.rm=T))
+}
+
+
+y_pred_add = y_decomp$trend + y_decomp$seasonal
+y_pred_mult = y_decomp_mult$trend * y_decomp_mult$seasonal 
+
+print(mape(y_pred_add, y$M2))
+print(mape(y_pred_mult, y$M2))
+
+#Trend and Seasonality in Additive Plot
+plot.ts(y_decomp$trend, main = "Additive Trend Plot (M2)", ylab = 'Trend',col = 'red')
+plot.ts(y_decomp$seasonal[1:12], main = "Additive Seasonality Plot (M2)", ylab = 'Seasonal Effect', col = 'blue')
+
+############################
+# Stationarity Adjustments #
+############################
+
+>>>>>>> Stashed changes
 # Testing for stationarity
-adf.test(y$cpi); kpss.test(y$cpi)
-adf.test(y$M2); kpss.test(y$M2)
+adf.test(y$CPI); kpss.test(y$CPI, null="Trend")
+adf.test(y$M2); kpss.test(y$M2, null="Trend")
 
 # Adjusting for seasonality
-cpi_smoothed = HoltWinters(y$cpi, alpha=0.1, beta=FALSE, gamma=FALSE)
+cpi_smoothed = HoltWinters(y$CPI, alpha=0.1, beta=FALSE, gamma=FALSE, seasonal="additive")
 cpi_smoothed = cpi_smoothed$fitted[,"xhat"]
-M2_smoothed = HoltWinters(y$M2, alpha=0.1, beta=FALSE, gamma=FALSE)
+M2_smoothed = HoltWinters(y$M2, alpha=0.1, beta=FALSE, gamma=FALSE, seasonal="additive")
 M2_smoothed = M2_smoothed$fitted[,"xhat"]
 
 for (i in ncol(y)){
@@ -52,4 +139,8 @@ grangertest(cpi ~ M2, data=y_diff) # p < 0.05 --- M2 needed to predict CPI
 grangertest(M2 ~ cpi, data=y_diff) # p > 0.05 --- CPI not needed to predict M2
 
 
-
+# plot.ts(y$CPI)
+# lines(cpi_smoothed, col="red")
+# 
+# plot.ts(y$M2)
+# lines(M2_smoothed, col="red")
